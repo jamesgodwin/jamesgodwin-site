1069689371fdcd3a187ed4da64152965db1a1dc2
James Godwin <james@madebythem.com>
2024-09-17 17:18:27 +0200

    Update code.js

--- src/outputs/code.js
+++ src/outputs/code.js
@@ -1,7 +1,7 @@
 const codeOutput = `
 <p>While design is my first love, I've found coding to be an unexpectedly creative pursuit. As a self-taught programmer with over two decades of experience, I've built a strong foundation in multiple coding languages and frameworks. This expertise has allowed me to create scalable, secure, and user-friendly applications, from developing mobile wallets and financial systems to building real-time online platforms. My coding skills are diverse and adaptive, always driven by the desire to craft meaningful digital experiences. Thanks to AI, I now code better and faster, with less struggle and more creativity.</p>
 
-<p>This websites <a href="https://github.com/jamesgodwin/jamesgodwin-site" target="_blank">code is available on GitHub</a>. Feel free to share and hekp me improve as this was my first React project.</p>
+<p>This websites <a href="https://github.com/jamesgodwin/jamesgodwin-site" target="_blank">code is available on GitHub</a>. Feel free to share and help me improve as this was my first React project.</p>
 
 <hr>
   <p class="commands-text">Available commands: - help - about - now - apps - books - portfolio - paintings  - uiux - contact - code - taoism<br />themes (default, snow, mist, zen, tao, mountain)  - clear</p>
