9eefe6bcd74921de71c98858d336b2942d226f93
James Godwin <james@madebythem.com>
2024-10-03 19:05:07 +0200

    updates

--- src/outputs/now.js
+++ src/outputs/now.js
@@ -1,13 +1,12 @@
 const nowOutput = `
 <p><strong>What I'm doing now</strong><br />
-<i>Updated <strong>September 17th, 2024</strong>, from my home in South Africa, where it's the start of Spring.</i></p>
+<i>Updated <strong>October 2nd, 2024</strong>, from my home in South Africa, where it's the start of Spring.</i></p>
 <ol>
 <li>Finished upgrading my personal site. Making some minor adjustments</li>
-<li>My day job is consulting to a UX agenecy in the US. iiimpact.io. Busy closing off on a "AI Powered Third Party Risk and Diligence Solutions" project and about to begin work on a "Managed Detection and Response Services" for cyber security.</li>
+<li>My day job is consulting to a UX agenecy in the US. iiimpact.io. Diving deep into the Cyber Security space. Working on a new exiting project.</li>
 <li>Ongoing promotion and creating videos for my apps. Building them was easy but marketing I find very challenging!</li>
 <li>Learning to play Paddle Tennis</li>
 <li>Summer is around the corner. Wind has begun blowing so soon be back on the water Kite Boarding"</li>
-<li>Writing a new book - worked more on it today</li>
 </ol>
 
 <hr>
